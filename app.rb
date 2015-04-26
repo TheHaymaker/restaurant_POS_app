09d634a3f5093b7ed7cd3743baf299b67fd42e2b
Brett Haymaker <brett.haymaker@gmail.com>
2015-04-26 14:52:06 -0500

    add routes, build PSQL database and create foods, parties and orders tables

--- app.rb
+++ app.rb
@@ -5,7 +5,7 @@ Bundler.require()
 # ** Connections **
 ActiveRecord::Base.establish_connection(
 	:adapter => 'postgresql',
-	:database => 'REST_app',
+	:database => 'rest_app',
 	:host => 'localhost'
 	)
 
@@ -17,5 +17,57 @@ require './models/party'
 # ** Helper functions? **
 
 # ** Routes **
+
+#  display waitstaff's application
+get '/' do 
+	erb :index
+end
+
+# all food items available
+get '/api/foods' do 
+	content_type :to_json
+	Food.all.to_json
+end
+
+#  get a single food item and all the parties that included it
+get '/api/foods/:id' do 
+food = Food.find(params[:id].to_i)
+ordersList = food.orders
+
+end
+
+
+#  Create a new food item
+
+# Update a food item (put)
+
+# update a food item (patch)
+
+# delete a food item
+
+# all the parties
+
+# a single party and all the orders it contains
+
+# create a new party
+
+# update a partys details (put)
+
+# update a partys details (patch)
+
+# delete a party
+
+# create a new order
+
+# change item to no-charge
+
+# removes an order
+
+# saves the partys receipt data to a file
+
+# marks the party as paid (put)
+
+# marks the party as paid (patch)
+
 
 
