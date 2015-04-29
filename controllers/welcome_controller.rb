d125fbb868c5f38b72a954917ecf9305af070fdf
Brett Haymaker <brett.haymaker@gmail.com>
2015-04-29 12:48:42 -0500

    Begins controller build with Welcome Controller and authenticated and non-authenticated pages

---
+++ controllers/welcome_controller.rb
@@ -0,0 +1,23 @@
+class WelcomeController < Sinatra::Base
+	enable :sessions
+	helpers Sinatra::SessionHelper
+
+	set :views, File.expand_path('../../views', __FILE__)
+	set :public_folder, File.expand_path('../../public', __FILE__)
+
+	get 'pry' do
+		binding.pry
+	end
+
+	get '/' do 
+		erb :index
+	end
+
+	get '/admin' do 
+		if current_user
+			erb :authenticated
+		else
+			erb :non_authenticated
+		end
+	end
+end
