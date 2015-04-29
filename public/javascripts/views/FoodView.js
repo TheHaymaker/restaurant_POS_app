6387950bd003c759ac32aa333d5de815cc2262c4
Brett Haymaker <brett.haymaker@gmail.com>
2015-04-29 14:58:47 -0500

    arranges Views in JS file architecture

---
+++ public/javascripts/views/FoodView.js
@@ -0,0 +1,30 @@
+
+	var app = app || {};
+
+// =============================================
+//           F O O D   V I E W 
+// =============================================
+
+app.FoodView = Backbone.View.extend({
+	initialize: function(){
+		this.listenTo(this.model, 'change', this.render);
+		this.listenTo(this.model, 'delete', this.remove);
+	},
+	tagName: 'li',
+	className: 'single-food',
+	template: _.template($('.food-display').html()),
+	render: function(){
+		var data = this.model.attributes;
+		this.$el.append( this.template( data ) );
+		return this;
+	},
+	events: {
+		'click .select-food': 'selectFood'
+	},
+	selectFood: function(){
+		$('.food-selected').removeClass('food-selected');
+		this.$el.addClass('food-selected');
+		app.foodSelection = this.model;
+	}
+
+});
---
+++ public/javascripts/views/PartyView.js
@@ -0,0 +1,38 @@
+	var app = app || {};
+
+//=============================================
+//           P A R T Y   V I E W
+// =============================================
+
+app.PartyView = Backbone.View.extend({
+	initialize: function(){
+		this.listenTo(this.model, 'change', this.render);
+		this.listenTo(this.model, 'delete', this.remove);
+	},
+	tagName: 'li',
+	className: 'single-party',
+	template: _.template($('.party-display').html()),
+	render: function(){
+		var data = this.model.attributes;
+		this.$el.html( this.template( data ) );
+		this.renderFoodList();
+		return this;
+	},
+	renderFoodList: function(){
+		var foods = this.model.get('foods');
+		var foodList = $('<ul>');
+		for (var i = 0; i < foods.length; i++) {
+			foodList.append( $('<li>').text(foods[i]['name'] ) );
+		}
+		this.$el.append(foodList);
+	},
+	events: {
+		'click .select-party': 'selectParty'
+	},
+	selectParty: function() {
+		$('.party-selected').removeClass('party-selected');
+		this.$el.addClass('.party-selected');
+		app.partySelection = this.model;
+	}
+
+});
